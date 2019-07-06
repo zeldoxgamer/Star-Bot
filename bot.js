9a1511ae6550ec1e94fc7b86a79856cfcb0c4f45
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-06 15:32:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,55 +136,7 @@ client.on('guildMemberAdd', async member => { // membed add event
 
  
 
-const say = JSON.parse(fs.readFileSync('./say.json', 'utf8'))
 
- 
-
-client.on("message", message =>
-
-          {
-
-      if(!say[message.guild.id]) say[message.guild.id] = {
-
-        say: 'say'
-
-        }
-
-  if(message.content.startsWith(prefix + "say" ) 
-
-  message.content.startsWith(say[message.guild.id].say)) {
-
-    var args = message.content.split(" ").slice(1).join(" ")
-
-    message.channel.send(args)
-
-  }});
-
-client.on("message", message => {
-
-if(message.content.startsWith(prefix + 'set-say')) {
-
-  var args = message.content.split(" ").slice(1).join(" ")
-
-    if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
-
-    if(!say[message.guild.id]) say[message.guild.id] = {
-
-        say: 'say'
-
-        }
-
-        message.channel.send(`**SET THE SAY COMMAND TO ${args}**`), say[message.guild.id].say = args
-
-        fs.writeFile("./say.json", JSON.stringify(say), (err) => {
-
-            if (err) console.error(err)
-
-        })
-
-}
-
-}) 
 
 
  client.on("message", message => {  //iTzMurtaja
