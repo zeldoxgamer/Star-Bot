86bc66e3c2d4ef0b6935258c02b3267ad3fb439f
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-07 10:41:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,21 @@
 
 });  
        
- PanDa Bot ServerInfo Code Lol joy 
+ client.on('message', message => {  
+	if (message.author.bot) return;
+if (message.content.startsWith(prefix + 'clear')) { //Codes
+	if(!message.channel.guild) return message.reply('⛔ | This Command For Servers Only!'); 
+		if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | You dont have **MANAGE_MESSAGES** Permission!');
+        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | I dont have **MANAGE_MESSAGES** Permission!');
+ let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 99) return message.reply("**🛑 || يجب ان يكون عدد المسح أقل من 100 .**").then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
+    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
+  }
+  });
+
     if(message.content.startsWith(prefix +"server")){ // الامر
       if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** ❎ `)
     if(!message.channel.guild) return message.reply(' ');
