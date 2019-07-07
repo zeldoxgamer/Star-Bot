60ea4c9b808e6489562eb2a84f038f58a441c832
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-07 02:44:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,35 @@
 
 }); 
 
-  
+  client.on('message', message => {
+ 
+    if (message.content === `${prefix}closeroom`) {
+                        if(!message.channel.guild) return message.reply(' This command only for servers');
+ 
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+ 
+           }).then(() => {
+               message.reply("تم تقفيل الشات :white_check_mark: ")
+           });
+             }
+//™¦༺♚ƙἶղց|MaS♚༺¦™#7105
+if (message.content === `${prefix}openroom`) {
+    if(!message.channel.guild) return message.reply(' This command only for servers');
+ 
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+ 
+           }).then(() => {
+               message.reply("تم فتح الشات:white_check_mark:")
+           });
+             }
+ 
+ 
+ 
+});
 
 client.on("message", message => {
 	var args = message.content.split(' ').slice(1); 
