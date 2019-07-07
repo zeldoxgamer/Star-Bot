a303c2efd0b457708eeabdcffddc63ed65ebce91
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-07 01:09:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,266 @@
  const prefix = "#";
  const token = 'BOT_TOKEN'; 
 
+client.on('message', message => {
+
+  if (message.author.x5bz) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+ 
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+ 
+
+  let args = message.content.split(" ").slice(1);
+
+ 
+
+  if (command == "ban") {
+
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+         
+
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+
+  let user = message.mentions.users.first();
+
+  let reason = message.content.split(" ").slice(2).join(" ");
+
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+
+ 
+
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+
+  if (!message.guild.member(user)
+
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+ 
+
+  message.guild.member(user).ban(7, user);
+
+ 
+
+  const banembed = new Discord.RichEmbed()
+
+  .setAuthor(`BANNED!`, user.displayAvatarURL)
+
+  .setColor("RANDOM")
+
+  .setTimestamp()
+
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+
+  message.channel.send({
+
+    embed : banembed
+
+  })
+
+}
+
+});
+
+const Discord = require("discord.js");
+
+const fs = require("fs");
+
+const ms = require("ms");
+
+let warns = JSON.parse(fs.readFileSync("./warnings.json"));;
+
+const client = new Discord.Client();
+
+ 
+
+ 
+
+client.on('message', async message => {
+
+ 
+
+  if (message.author.x5bz) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+ 
+
+ 
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+ 
+
+  let args = message.content.split(" ").slice(1);
+
+ 
+
+  if (command == "warn") { //??? ???????
+
+ 
+
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+         
+
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+
+  let user = message.mentions.users.first();
+
+  let reason = message.content.split(" ").slice(2).join(" ");
+
+ 
+
+  if (message.mentions.users.size < 1) return message.reply("**???? ???**");
+
+  if(!reason) return message.reply ("**???? ??? ?????**");
+
+ 
+
+ 
+
+  if(!warns[user.id]) warns[user.id] = {
+
+    warns: 0
+
+  };
+
+ 
+
+  warns[user.id].warns++;
+
+ 
+
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+
+    if (err) console.log(err)
+
+  });
+
+ 
+
+ 
+
+  const banembed = new Discord.RichEmbed()
+
+  .setAuthor(`WARNED!`, user.displayAvatarURL)
+
+  .setColor("RANDOM")
+
+  .setTimestamp()
+
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+
+   client.channels.find('name', 'log').send({
+
+    embed : banembed
+
+  })
+
+ 
+
+    if(warns[user.id].warns == 2){ //??? ???????? ??????
+
+    let muterole = message.guild.roles.find(`name`, "Muted");
+
+    if(!muterole){
+
+      try{
+
+        muterole = await message.guild.createRole({
+
+          name: "Muted",
+
+          color: "#000000",
+
+          permissions:[]
+
+        })
+
+        message.guild.channels.forEach(async (channel, id) => {
+
+          await channel.overwritePermissions(muterole, {
+
+            SEND_MESSAGES: false,
+
+            ADD_REACTIONS: false
+
+          });
+
+        });
+
+      }catch(e){
+
+        console.log(e.stack);
+
+      }
+
+    }
+
+   
+
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    if(!tomute) return message.reply("**??? ???? ?????? ?????**:x: ") .then(m => m.delete(5000));
+
+   
+
+    let mutetime = "60s";
+
+    await(tomute.addRole(muterole.id));
+
+    message.channel.send(`<@${user.id}> has been temporarily muted`);
+
+ 
+
+    setTimeout(async function(){
+
+    await(tomute.removeRole(muterole.id));
+
+      message.reply(`<@${user.id}> has been unmuted.`)
+
+    }, ms(mutetime))
+
+  }
+
+  if(warns[user.id].warns == 3){  //??? ???????? ??????
+
+    message.guild.member(user).ban(reason);
+
+    message.reply(`<@${user.id}> has been banned.`)
+
+  }
+
+ 
+
+}
+
+}
+
+);
+
 
 
 
@@ -382,6 +642,7 @@ let embed = new Discord.RichEmbed()
 .addField(`     **${prefix}closeroom**  ` ,' **لاغلاق الشات** ')
 .addField(`     **${prefix}openroom** ` ,' **لفتح الشات** ')
 .addField(`     **${prefix}dar**  ` ,' **لمسح جميع الرومات** ')
+.addField(`     **${prefix}server** ` , ' **لمعرفة معلومات عن السيرفر** ')
 .setColor('RED')
 message.author.sendEmbed(embed);
 }
