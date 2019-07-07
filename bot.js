d90f926a41d8aa48c4061ec5b8b7754b8eddfa72
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-07 15:38:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,103 @@ message.guild.createChannel(name, `text`).then(c => {
             });
         }).catch(console.error);
     }
+	 if(message.content.startsWith(prefix + 'close')) {
+     
+     if(message.author.bot) return;
+       if(!message.channel.name.startsWith("ticket-")) return message.channel.send(`this command only for the tickets`)
+ let close = new Discord.RichEmbed()
+ .addField(`**اكتب \`${prefix}close\` مجددا للتأكيد**`, `** **`)
+ .setColor("#36393e");
+ message.channel.sendEmbed(close) .then(m => {
+ const filter = msg => msg.content.startsWith(prefix + 'close');
+ if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return
+ message.channel.awaitMessages(response => response.content === prefix + 'close', {
+ max: 1,
+ time: 20000,
+ errors: ['time']
+ })
+ .then((collect) => {
+ message.channel.delete();
+ let Reason = message.content.split(" ").slice(1).join(" ");
+ if(!Reason) Reason = 'NONE';
+let closee = new Discord.RichEmbed()
+.setColor(`BLUE`)
+.setAuthor(`تم اغلاق التيكت`)
+.setDescription(`Ticket : #${message.channel.name}
+By : <@${message.author.id}>
+Reason : ${Reason}`)
+.setTimestamp()
+.setThumbnail(`https://cdn.discordapp.com/attachments/584630360017469461/588033109178712074/563111850162520077.png`)
+.setFooter(message.author.tag)
+let log = message.guild.channels.find("name", "log");
+if(log) log.send(closee)
+ }) .catch(() => {
+ m.delete()
+ .then(message.channel.send('**تم اغلاق التيكت**')) .then((c) => {
+ c.delete(4000);
+ })
+ })
+ })    
+   } if(message.content.startsWith(prefix + `multiclose`)) {
+     if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.channel.send(`**Error** :octagonal_sign:\nI Don\'t have MANAGE_CHANNELS Permission`)
+     if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply('You don\'t have Permission **MANAGE_CHANNELS** to close all tickets');
+      message.guild.channels.filter(c => c.name.toLowerCase().startsWith("ticket-")).forEach(channel => { channel.delete(); })
+const ttt = new Discord.RichEmbed()
+.setColor("GREEN")
+.addField(`**Done all Tickets has been closed :white_check_mark:**`,`** **`)
+message.channel.send(ttt)
+let log = message.guild.channels.find("name", "log");
+const rr = new Discord.RichEmbed()
+.setColor("GREEN")
+.addField(`**تم مسح جميع التيكتات:white_check_mark:**`, `**by <@${message.author.id}>**`)
+.setThumbnail(`https://cdn.discordapp.com/attachments/584630360017469461/588151961279397898/582096914376425501.png`)
+.setTimestamp();
+if(log) return log.send(rr)
+//
+} if(message.content.startsWith(prefix + `add`)) {
+  if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.channel.send(`**Error** :octagonal_sign:\nI Don\'t have MANAGE_CHANNELS Permission to do this`)
+ if(!message.channel.name.startsWith("ticket-")) return message.channel.send(`this command only for the tickets`);
+let member = message.mentions.members.first();
+if(!member) return message.channel.send(`**Please mention the user :x:**`);
+if(message.channel.permissionsFor(member).has(['SEND_MESSAGES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'])) return message.channel.send(`this member already in this ticket :rolling_eyes:`);
+message.channel.overwritePermissions(member.id, { SEND_MESSAGES: true, VIEW_CHANNEL: true, READ_MESSAGE_HISTORY: true });
+message.channel.send(`**Done :white_check_mark:\nSuccessfully added <@${member.user.id}> to the ticket**`)
+let tgt = new Discord.RichEmbed()
+.setColor(`GREEN`)
+.setAuthor(`تم اضافته الى التدكرة`)
+.setDescription(`Ticket : #${message.channel.name}
+Member : ${member}
+by : <@${message.author.id}>`)
+.setThumbnail(`https://cdn.discordapp.com/attachments/584630360017469461/588033109539160066/563111851165220885.png`)
+.setTimestamp();
+let log = message.guild.channels.find("name", "log");
+if(log) return log.send(tgt);
+} if(message.content.startsWith(prefix + `remove`)) {
+ if(!message.channel.name.startsWith("ticket-")) {
+     return message.channel.send(`this command only for the tickets`);
+ }
+ let member = message.mentions.members.first();
+ if(!member || member.id === client.user.id) {
+     return message.channel.send(`**Please mention the user :x:**`);
+ }
+ if(!message.channel.permissionsFor(member).has(['SEND_MESSAGES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'])) {
+     return message.channel.send(`:x: **${member.user.tag}** is not in this ticket to remove them`);
+ }
+ message.channel.overwritePermissions(member.id, { SEND_MESSAGES: false, VIEW_CHANNEL: false, READ_MESSAGE_HISTORY: false });
+ message.channel.send(`**Done :white_check_mark:\nSuccessfully removed \`${member.user.tag}\` from the ticket**`)
+ let gtg = new Discord.RichEmbed()
+.setColor(`BLUE`)
+.setAuthor(`تم استبعاده من التدكرة`)
+.setDescription(`Ticket : #${message.channel.name}
+Member : ${member}
+by : <@${message.author.id}>`)
+.setThumbnail(`https://cdn.discordapp.com/attachments/584630360017469461/588033111212949555/563111852352077886.png`)
+.setTimestamp();
+let log = message.guild.channels.find("name", "log");
+if(log) return log.send(gtg);
+ }
+ 
+ 
  });
 
 
