ec684a703e309d01fbbd822c11666257dbaa9bce
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-07 01:46:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,81 +7,37 @@
  const prefix = "#";
  const token = 'BOT_TOKEN'; 
 
+
 client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  if (message.author.x5bz) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
-  if (!message.content.startsWith(prefix)) return;
+  let args = message.content.split(" ").slice(1);
 
- 
+  if (command == "ban") {
+    if (!message.channel.guild) return;
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**Mention Someone**");
+  if(!reason) return;
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**This person has a grade higher than his bot rank**");
 
-  let command = message.content.split(" ")[0];
+  message.guild.member(user).ban(7, user);
+  message.channel.send(`**:white_check_mark: ${user} تم اعطائه باند :airplane: **`)
+user.send(`تم اعطائك باند في ${message.guild.name} السبب: ${reason}`)
+  }})
 
-  command = command.slice(prefix.length);
 
- 
 
-  let args = message.content.split(" ").slice(1);
-
- 
-
-  if (command == "ban") {
-
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-         
-
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-
-  let user = message.mentions.users.first();
-
-  let reason = message.content.split(" ").slice(2).join(" ");
-
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-
- 
-
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-
-  if (!message.guild.member(user)
-
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
- 
-
-  message.guild.member(user).ban(7, user);
-
- 
-
-  const banembed = new Discord.RichEmbed()
-
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-
-  .setColor("RANDOM")
-
-  .setTimestamp()
-
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-
-  message.channel.send({
-
-    embed : banembed
-
-  })
-
-}
-
-});
 
 
 
