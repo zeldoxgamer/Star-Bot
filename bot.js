a87ac77bf31a51115eac7c441afd2087ff8eeb76
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-08 15:50:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@
  const dateFormat = require('dateformat');
  const client = new Discord.Client(); 
  const Canvas = require("canvas"); 
+ const canvas = require("canvas");
  const prefix = "#";
  const token = 'BOT_TOKEN'; 
  const version = "1.1.2";
