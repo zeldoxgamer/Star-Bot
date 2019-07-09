6b093b8de94b524739a8100618c96f0624264f0e
zeldoxgamer <45310372+zeldoxgamer@users.noreply.github.com>
2019-07-09 11:30:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -951,7 +951,7 @@ client.on('message', message => {
   let embed = new Discord.RichEmbed()
 .setAuthor(message.author.username)
 .setColor("#9B59B6")
-.addField(" **اصدار اابوت هو**" , `Version: ${version})
+.addField(" **اصدار اابوت هو**" , `Version: ${version}`)
   
  message.channel.sendEmbed(embed);
  }
